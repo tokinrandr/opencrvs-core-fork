68d3961c666bea511fa436db6ca123ca0c04b337
Pyry <pyry.rouvila@gmail.com>
2024-05-29 10:18:50 +0300

    feat: fetch location ids for child locations and search with them

--- packages/metrics/src/features/certifications/handler.ts
+++ packages/metrics/src/features/certifications/handler.ts
@@ -14,7 +14,10 @@ import {
   TIME_TO,
   LOCATION_ID
 } from '@metrics/features/metrics/constants'
-import { getTotalCertifications } from '@metrics/features/certifications/service'
+import {
+  getAllTotalCertifications,
+  getTotalCertifications
+} from '@metrics/features/certifications/service'
 
 export async function totalCertificationsHandler(
   request: Hapi.Request,
@@ -23,8 +26,10 @@ export async function totalCertificationsHandler(
   const timeStart = request.query[TIME_FROM]
   const timeEnd = request.query[TIME_TO]
   const locationId = request.query[LOCATION_ID]
-    ? 'Location/' + request.query[LOCATION_ID]
-    : undefined
 
-  return getTotalCertifications(timeStart, timeEnd, locationId)
+  if (locationId) {
+    return getAllTotalCertifications(timeStart, timeEnd)
+  } else {
+    return getTotalCertifications(timeStart, timeEnd, locationId)
+  }
 }
--- packages/metrics/src/features/certifications/service.ts
+++ packages/metrics/src/features/certifications/service.ts
@@ -9,32 +9,45 @@
  * Copyright (C) The OpenCRVS Authors located at https://github.com/opencrvs/opencrvs-core/blob/master/AUTHORS.
  */
 
+import { UUID } from '@opencrvs/commons'
 import { query } from '@metrics/influxdb/client'
+import { fetchChildLocationIds } from '@metrics/utils/location'
+
+export async function getAllTotalCertifications(
+  timeFrom: string,
+  timeTo: string
+) {
+  return query<Array<{ total: number; eventType: string }>>(
+    `SELECT COUNT(DISTINCT(compositionId)) AS total
+      FROM certification
+    WHERE time > $timeFrom
+      AND time <= $timeTo
+    GROUP BY eventType`,
+    {
+      placeholders: {
+        timeFrom,
+        timeTo
+      }
+    }
+  )
+}
 
 export async function getTotalCertifications(
   timeFrom: string,
   timeTo: string,
-  locationId: string | undefined
+  locationId: UUID
 ) {
+  const locationIds = await fetchChildLocationIds(locationId)
   const totalMetrics = await query<Array<{ total: number; eventType: string }>>(
     `SELECT COUNT(DISTINCT(compositionId)) AS total
       FROM certification
     WHERE time > $timeFrom
       AND time <= $timeTo
-      ${
-        locationId
-          ? `AND ( locationLevel1 = $locationId
-      OR locationLevel2 = $locationId      
-      OR locationLevel3 = $locationId
-      OR locationLevel4 = $locationId
-      OR locationLevel5 = $locationId
-      OR officeLocation = $locationId)`
-          : ``
-      }
+      AND officeLocation IN ($locationIds)
     GROUP BY eventType`,
     {
       placeholders: {
-        locationId,
+        locationIds,
         timeFrom,
         timeTo
       }
